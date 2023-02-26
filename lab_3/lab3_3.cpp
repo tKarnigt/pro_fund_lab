4a94d6d5a4cf15475cdea14e89c4ab401199f784
Krit Tangpinyoputtikhun <88623313+jino1205101@users.noreply.github.com>
2023-02-26 19:08:42 +0700

    Create lab3_3.cpp

---
+++ lab_3/lab3_3.cpp
@@ -0,0 +1,22 @@
+#include "stdio.h"
+int main(){
+  int num;
+  scanf("%d", &num);
+  for(int i=1;i<=num-1;i++){      //PART 1
+    for(int j=1;j<=i;j++)printf("*");
+    for(int j=1;j<=(num*2-1)-(i*2);j++)printf(" ");
+    for(int j=1;j<=i;j++)printf("*");
+    printf("\n");
+  }
+
+  for(int i=1;i<=2*num-1;i++)printf("*");   //PART 2
+  printf("\n");
+
+  for(int i=1;i<=num-1;i++){      //PART3
+    for(int j=1;j<=num-i;j++)printf("*");
+    for(int j=1;j<=i*2-1;j++)printf(" ");
+    for(int j=1;j<=num-i;j++)printf("*");
+    printf("\n");
+  }
+  return 0;
+}
